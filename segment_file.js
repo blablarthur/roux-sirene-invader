885b67dda53249780ec01c6d3c2a272035096535
Arthur Roux <arthur.roux@epita.fr>
2021-11-19 17:47:50 +0100

    choose chunk of lines to copy into file in 5s, with memory leak

--- segment_file.js
+++ segment_file.js
@@ -1,17 +1,38 @@
 import * as fs from 'fs';
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'process';
+import * as readline from 'readline';
 
 export const segment_file = (filePath) => {
     if (typeof filePath !== "string")
         throw 'filePath is not correct.';
     if (!fs.existsSync(filePath))
         throw "filePath don't exist.";
-    const rl = readline.createInterface({ input, output });
-    const answer = await rl.question('What do you think of Node.js? ');
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-
-    rl.close();
+    const start = Date.now();
+    const readableStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(TEXT_OUTPUT_PATH);
+    const rl = readline.createInterface({
+        input: readableStream
+    });
+    let i = 0;
+    rl.on('line', (input) => {
+        if (i - 1 >= CHUNK_LINE_READ * CHUNK_TO_READ_FOR_100M_FILE) {
+            writeStream.write(input + "\n");
+            if (i >= CHUNK_LINE_READ * CHUNK_TO_READ_FOR_100M_FILE * 2) {
+                rl.removeAllListeners();
+                rl.close();
+                writeStream.end();
+                readableStream.destroy();
+                const millis = Date.now() - start;
+                console.log(`seconds elapsed = ${Math.round(millis / 1000)}`);
+            }
+        }
+        i++;
+    });
 }
 
-console.log(segment_file("/home/arthur/workspace/EPITA/roux-sirene-invader/tests/testFiles/test.txt"));
+const TEXT_INPUT_PATH = "/home/arthur/workspace/EPITA/roux-sirene-invader/tests/testFiles/test.txt";
+const TEXT_OUTPUT_PATH = "/home/arthur/workspace/EPITA/roux-sirene-invader/tests/testFiles/testOutput.txt";
+const STOCK_ESTABLISHEMENT_PATH = "/mnt/c/Users/EDF/Downloads/StockEtablissement_utf8/StockEtablissement_utf8.csv";
+const CHUNK_LINE_READ = 429;
+const CHUNK_TO_READ_FOR_100M_FILE = 1449;
+
+segment_file(STOCK_ESTABLISHEMENT_PATH);
